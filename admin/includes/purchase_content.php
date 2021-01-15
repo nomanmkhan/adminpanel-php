9d116feafe9d61605698138a5f03dc299a219fd8
nomanmkhan <nomanmuhammadkhan@gmail.com>
2021-01-15 16:10:16 +0500

    second commit- with product values addition with tax

--- admin/includes/purchase_content.php
+++ admin/includes/purchase_content.php
@@ -9,7 +9,7 @@
         <form id="form_tbll" method="post">
             <section class="content">
                 <div class="row">
-                    <div class="col-lg-6">
+                    <div class="col-lg-5">
                         <table class="table table-dark table-striped">
                             <thead>
                                 <tr>
@@ -42,7 +42,7 @@
 
 
                     <!-- SECOND TABLE TO APEND -->
-                    <div class="col-lg-6" id="invoice">
+                    <div class="col-lg-7" id="invoice">
                         <table class="table table-dark table-striped">
                             <thead>
                                 <tr>
@@ -51,6 +51,7 @@
                                     <th>Price</th>
                                     <th>Quantity </th>
                                     <th>total</th>
+                                    <th>action</th>
 
                                 </tr>
                             </thead>
@@ -58,6 +59,7 @@
 
                             </tbody>
                         </table>
+
 
                     </div>
                 </div>
@@ -69,62 +71,162 @@
 </div>
 
 <table style="display: none;">
-        <tbody id="right_div">
-            <tr class="right_row">
-                <td class="idt"></td>
-                <td class="name"></td>
-                <td class="sell_price"></td>
-                <td><input type='number' name="quantity" class="quantity" id="sel_quantity" value="0"></td>
-                <td><input class="total" name="ind_total" value="0" readonly></td>
-            
-                <!-- <td><button class="btn btn-danger remove_btn"><i class="fa fa-fw fa-minus"></i> Remove Item</button></td> -->
-            </tr>
-        </tbody>
-    </table>
+    <tbody id="right_div">
+        <tr class="right_row">
+            <td class="idt"></td>
+            <td class="name"></td>
+            <td class="sell_price"></td>
+            <td><input type='number' name="quantity[]" class="quantity" id="sel_quantity" value="0"></td>
+            <td><input class="total" name="ind_total[]" value="0" readonly></td>
+
+            <td><button type="button" class="btn btn-danger remove_btn">Remove</button></td>
+        </tr>
+    </tbody>
+</table>
+
+
+
+<div class="row">
+    <div class="col-lg-6"></div>
+    <div class="col-lg-6">
+        <label>Total Items</label> <input style="margin-left: 20px;" name="total_items" type="number" id="stock_quant" value="0" readonly></br>
+        <label>Tax Payable</label> <input style="margin-left: 14px;" name="tax" type="number" id="tax" readonly></br>
+        <label>Total Price</label> <input style="margin-left: 23px;" name="total_price" type="number" id="sel_quantity" value="0" readonly>
+    </div>
+</div>
+
+
+
+
 <script>
+    var id_array = [];
+
+
+
+    $('#right_tbl').on('click', '.remove_btn', function() {
+        var id = $(this).attr('data-id');
+        // console.log(id);
+
+        $('#right_row_' + id).remove();
+        var removeItem = id;
+
+        id_array = jQuery.grep(id_array, function(value) {
+            return value != removeItem;
+            // console.log(value);
+        });
+
+        for (var i = 0; i < id_array.length; i++) {
+            id = id_array[i];
+
+            var priceTotal = $('#total_' + id).val();
+            var total_stock = parseInt(total_stock) + parseInt(quantity);
+            var total = parseInt(total) + parseInt(priceTotal);
+
+        }
+        var tax = total * 0.2;
+        total = total + tax;
+
+        $('#tax').val(tax);
+        $('#stock_quant').val(total_stock);
+        $('#sel_quantity').val(total);
+
+    });
+
+
+
 
 
     $('.add_btn').on('click', function() {
         var id = $(this).attr('data-id');
-        console.log(id);
-        $('#right_div').find('.right_row').attr('id', 'right_row_' + id);
-        $('#right_div').find('.idt').html(id);
-        
-        var name = $('#name_' + id).html();
-        $('#right_div').find('.name').html(name);
 
-        var sell_price = $("#sell_price_" + id).html();
-        $('#right_div').find(".sell_price").html(parseInt(sell_price));
-
-        $('#right_div').find('.quantity').attr('id', 'quantity_' + id);
-        // $('#right_div').find('#id_hid').val($id);
-        $('#right_div').find('.quantity').attr('data-id', id);
-
-        
-        $('#right_div').find('.total').attr('id', 'total_' + id);
+        var ch = 1;
+        for (var i = 0; i < id_array.length; i++) {
+            if (id == id_array[i]) {
+                alert("Product Already there!!");
+                ch = 0;
+            }
+        }
+        if (ch == 1) {
+            id_array.push(id);
 
 
 
+            $('#right_div').find('.right_row').attr('id', 'right_row_' + id);
+            $('#right_div').find('.idt').html(id);
 
-        $('#right_tbl').append($('#right_div').html());
+            var name = $('#name_' + id).html();
+            $('#right_div').find('.name').html(name);
+
+            var sell_price = $("#sell_price_" + id).html();
+            $('#right_div').find(".sell_price").html(parseInt(sell_price));
+
+            $('#right_div').find('.quantity').attr('id', 'quantity_' + id);
+            $('#right_div').find('.quantity').attr('data-id', id);
+
+
+            $('#right_div').find('.total').attr('id', 'total_' + id);
+
+            $('#right_div').find('.remove_btn').attr('data-id', id);
+            $('#right_tbl').append($('#right_div').html());
+
+        }
 
     });
 
 
 
-    $('#right_tbl').on('change keyup', '.quantity', function () {
-    var quantity = this.value;
-    console.log(quantity);
-    var id = $(this).attr('data-id');
-    
-    console.log(id);
-    let sell_price = $("#sell_price_" + id).html();
-    console.log(sell_price);
 
-    
-    var new_total = parseInt((sell_price)) * quantity;
-    console.log(new_total);
-     $('#total_'+id).val(new_total);
+
+
+
+
+    $('#right_tbl').on('change keyup', '.quantity', function() {
+        var quantity = this.value;
+        // console.log(quantity);
+        var id = $(this).attr('data-id');
+
+        // console.log(id);
+        let sell_price = $("#sell_price_" + id).html();
+        // console.log(sell_price);
+
+
+        var new_total = parseInt((sell_price)) * quantity;
+        // console.log(new_total);
+        $('#total_' + id).val(new_total);
+
+
+        var tax = 0;
+        var total = 0;
+        var total_stock = 0;
+
+        for (var i = 0; i < id_array.length; i++) {
+            id = id_array[i];
+            // console.log(id);
+
+            // if ($('#quantity_' + id).val() == '') {
+            //     $('#quantity_' + id).val(parseInt(0));
+            // }
+
+            var priceTotal = $('#total_' + id).val();
+            // console.log("a: " + priceTotal);
+
+            //  quantity = $('#quantity_' + id).val();
+
+            // console.log(quantity);
+
+            var total_stock = parseInt(total_stock) + parseInt(quantity);
+            // console.log("total_stock=" + total_stock);
+
+            var total = parseInt(total) + parseInt(priceTotal);
+            // console.log("total=" + total);
+        }
+        var tax = total * 0.2;
+        total = total + tax;
+
+        $('#tax').val(tax);
+        $('#stock_quant').val(total_stock);
+        $('#sel_quantity').val(total);
+
+
     });
-
 </script>
